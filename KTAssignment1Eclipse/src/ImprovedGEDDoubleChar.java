e29e0525c127020f29c9fcfce5f0d61d333addac
Kaiqi Yang <kaiqiy1994@gmail.com>
2017-03-25 16:55:21 +1100

    [KT Assignment1 ] -Formatting

--- KTAssignment1Eclipse/src/ImprovedGEDDoubleChar.java
+++ KTAssignment1Eclipse/src/ImprovedGEDDoubleChar.java
@@ -52,20 +52,20 @@ public class ImprovedGEDDoubleChar {
 		CharMaps.put(GetMapKey('v', 'w'), (float) 0.0);
 		// v u
 		CharMaps.put(GetMapKey('v', 'u'), (float) 0.0);
-		
+
 		// f ph
 		CharMaps.put(GetMapKey("f", "ph"), (float) 0.0);
-		//CharMaps.put(GetMapKey("ph", "f"), (float) 0.0);
+		// CharMaps.put(GetMapKey("ph", "f"), (float) 0.0);
 		// y ie
 		CharMaps.put(GetMapKey("y", "ie"), (float) 0.0);
-		//CharMaps.put(GetMapKey("ie", "y"), (float) 0.0);
+		// CharMaps.put(GetMapKey("ie", "y"), (float) 0.0);
 		// c ch
 		CharMaps.put(GetMapKey("c", "ch"), (float) 0.0);
 		// k ck
 		CharMaps.put(GetMapKey("c", "ck"), (float) 0.0);
-		// ks x	
+		// ks x
 		CharMaps.put(GetMapKey("ks", "x"), (float) 0.0);
-		}
+	}
 
 	public String GetMapKey(char c1, char c2) {
 		return GetMapKey(Character.toString(c1), Character.toString(c2));
@@ -77,13 +77,13 @@ public class ImprovedGEDDoubleChar {
 
 	private float GlobalEditDist(String s1, String s2, Character preCharOfS1) {
 
-		float matchCharStrDist = Float.MAX_VALUE; // mapping on string 
-		float matchStrCharDist = Float.MAX_VALUE; // mapping on string 
-		float matchCharDist; // Edit distance if first char. match or do a replace
-		float insertDist; // Edit distance if insert first char of s1 in front of s2.
+		float matchCharStrDist = Float.MAX_VALUE; // mapping on string
+		float matchStrCharDist = Float.MAX_VALUE; // mapping on string
+		float matchCharDist; // Edit distance if first char. match or do a
+								// replace
+		float insertDist; // Edit distance if insert first char of s1 in front
+							// of s2.
 		float deleteDist; // Edit distance if delete first char of s2.
-		
-		
 
 		if (s1.length() == 0)
 			return insertString(s2); // Insert the remainder of s2
@@ -96,47 +96,48 @@ public class ImprovedGEDDoubleChar {
 			if (result != null) // Did we find the subproblem in the map?
 				return result; // If so, return the answer
 			else {
-				
-				// try using char to string 
-				String oldChar = s1.substring(0,1).toLowerCase();
+
+				// try using char to string
+				String oldChar = s1.substring(0, 1).toLowerCase();
 				String newStr = s2.length() > 2 ? s2.substring(0, 2).toLowerCase()
-						:s2.length() == 2 ?s2.toLowerCase(): "";
-				
-				String strMapKey = 	GetMapKey(oldChar, newStr);
-				
-				if(CharMaps.containsKey(strMapKey))
-				{
+						: s2.length() == 2 ? s2.toLowerCase() : "";
+
+				String strMapKey = GetMapKey(oldChar, newStr);
+
+				if (CharMaps.containsKey(strMapKey)) {
 					// map on string
-					matchCharStrDist = GlobalEditDist(s1.substring(1), s2.substring(2), s1.charAt(0)) + GetReplaceCost(oldChar, newStr);
+					matchCharStrDist = GlobalEditDist(s1.substring(1), s2.substring(2), s1.charAt(0))
+							+ GetReplaceCost(oldChar, newStr);
 				}
-				
-				// try using string to char 
+
+				// try using string to char
 				String oldStr = s1.length() > 2 ? s1.substring(0, 2).toLowerCase()
-						:s1.length() == 2 ?s1.toLowerCase(): ""; 
-						
-				String newChar = s2.substring(0,1);
-				
-				String strCharMapKey = 	GetMapKey(oldStr, newChar);
-				
-				if(CharMaps.containsKey(strCharMapKey))
-				{
+						: s1.length() == 2 ? s1.toLowerCase() : "";
+
+				String newChar = s2.substring(0, 1);
+
+				String strCharMapKey = GetMapKey(oldStr, newChar);
+
+				if (CharMaps.containsKey(strCharMapKey)) {
 					// map on string
-					matchStrCharDist = GlobalEditDist(s1.substring(2), s2.substring(1), s1.charAt(1)) + GetReplaceCost(oldStr, newChar);
+					matchStrCharDist = GlobalEditDist(s1.substring(2), s2.substring(1), s1.charAt(1))
+							+ GetReplaceCost(oldStr, newChar);
 				}
-				
+
 				// match on char
 				matchCharDist = GlobalEditDist(s1.substring(1), s2.substring(1), s1.charAt(0));
 				if (Character.toLowerCase(s1.charAt(0)) != Character.toLowerCase(s2.charAt(0))) {
 					matchCharDist = matchCharDist
-							+ GetReplaceCost(Character.toLowerCase(s1.charAt(0)),Character.toLowerCase( s2.charAt(0)));
-				} 
-				else {
+							+ GetReplaceCost(Character.toLowerCase(s1.charAt(0)), Character.toLowerCase(s2.charAt(0)));
+				} else {
 					matchCharDist = matchCharDist + GetMatchCost();
 				}
-				insertDist = GlobalEditDist(s1.substring(1), s2, s1.charAt(0)) + GetInsertCost(s1.charAt(0), preCharOfS1);
+				insertDist = GlobalEditDist(s1.substring(1), s2, s1.charAt(0))
+						+ GetInsertCost(s1.charAt(0), preCharOfS1);
 				deleteDist = GlobalEditDist(s1, s2.substring(1), preCharOfS1) + GetDeleteCost(s2.charAt(0));
 
-				float dist = Math.min(matchCharDist, Math.min(insertDist, Math.min(deleteDist, Math.min(matchCharStrDist, matchStrCharDist))));
+				float dist = Math.min(matchCharDist,
+						Math.min(insertDist, Math.min(deleteDist, Math.min(matchCharStrDist, matchStrCharDist))));
 
 				// System.out.println("Selected dist " + dist + "m, i, d = "
 				// matchDist +","+ insertDist + ", "+ deleteDist);
@@ -149,8 +150,7 @@ public class ImprovedGEDDoubleChar {
 
 	public float insertString(String str) {
 		float dist = 0;
-		if(str.length() >0)
-		{
+		if (str.length() > 0) {
 			Character preChar = str.charAt(0);
 			for (Character character : str.toCharArray()) {
 				dist += GetInsertCost(character, preChar);
@@ -171,8 +171,8 @@ public class ImprovedGEDDoubleChar {
 	public float GetMatchCost() {
 		return 0;
 	}
-	
-	public float GetReplaceCost(Character oldChar, Character newChar){
+
+	public float GetReplaceCost(Character oldChar, Character newChar) {
 		return GetReplaceCost(Character.toString(oldChar), Character.toString(newChar));
 	}
 
@@ -181,41 +181,39 @@ public class ImprovedGEDDoubleChar {
 		if (CharMaps.containsKey(GetMapKey(oldStr, newStr))) {
 			return CharMaps.get(GetMapKey(oldStr, newStr));
 		}
-		
-		if(oldStr.length() == 1 && newStr.length() == 1)
-		{
+
+		if (oldStr.length() == 1 && newStr.length() == 1) {
 			char oldc = oldStr.charAt(0);
 			char newc = newStr.charAt(0);
 			// vowels are kind of interchangeable
 			if (vowelSet.contains(oldc) && vowelSet.contains(newc)) {
 				return 1;
 			}
-			
+
 			// vowels replaced by non-vowels will be punished
 			if ((vowelSet.contains(oldc) && !vowelSet.contains(newc))
 					|| (!vowelSet.contains(oldc) && vowelSet.contains(newc))) {
 				return 3;
 			}
-			
+
 			return 2;
 		}
-		
+
 		return 2;
 	}
 
 	public float GetInsertCost(char insertChar, Character preChar) {
-		
-		if(preChar != null &&  preChar.charValue() == insertChar )
-		{
+
+		if (preChar != null && preChar.charValue() == insertChar) {
 			return (float) 0.5;
 		}
-		
+
 		if (vowelSet.contains(insertChar)) {
 			return 1;
 		}
-//		 if(plosiveSet.contains(insertChar)){
-//			 return (float) 3.1;
-//		 }
+		// if(plosiveSet.contains(insertChar)){
+		// return (float) 3.1;
+		// }
 		return 2;
 	}
 
@@ -313,43 +311,42 @@ public class ImprovedGEDDoubleChar {
 		// Adding data for the interchangeable letters yi, vo
 		// when the testSize = 100 the 46/100
 		// when the testSize = 200 the 81/200
-		
+
 		// Change the order of the conditions in the GetReplaceCost
 		// when the testSize = 100 the 46/100
 		// when the testSize = 200 the 86/200
-		
-		// Adding String matching 
+
+		// Adding String matching
 		// when the testSize = 100 the 51/100
 		// when the testSize = 200 the 96/200
 		// when the textSize = 300 the 131/300 43%
-		
-		// Adding the k and x mapping 
+
+		// Adding the k and x mapping
 		// when the textSize = 300 the 134/300 44%
-		
+
 		// Adding the v and w mapping (300 - 400)
 		// when the textSize = 100 the 38/100 (from 37)
-		
+
 		// Adding the v and u mapping (300 - 400)
-		// when the textSize = 100 the 41/100 
-		
+		// when the textSize = 100 the 41/100
+
 		// Adding the k and c mapping (400 - 500)
 		// when the textSize = 100 the 44/100 (from 42)
-		
-		// Adding the ks and x mapping (400 - 500) (from 44) 
+
+		// Adding the ks and x mapping (400 - 500) (from 44)
 		// when the textSize = 100 the 47/100
-		
+
 		int lowerB = 400;
 		int upperB = 500;
-		
+
 		int CorrectSize = 0;
 		int i = 0;
 		for (SimpleEntry<String, String> trainPair : trainData) {
 
-			if(i++ <= lowerB )
-			{
+			if (i++ <= lowerB) {
 				continue;
 			}
-			
+
 			if (i >= upperB) {
 				break;
 			}
