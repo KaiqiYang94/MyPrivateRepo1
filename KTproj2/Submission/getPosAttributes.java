05f4d0b7ef74ae3f167e21bb679237c923323392
Kaiqi Yang <kaiqiy1994@gmail.com>
2017-05-21 18:22:46 +1000

    [KT proj2] - to refine program

---
+++ KTproj2/Submission/getPosAttributes.java
@@ -0,0 +1,289 @@
+import java.util.*;
+import java.io.File;
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+class getPosAttributes {
+
+	static String negative = "negative";
+	static String neutral = "neutral";
+	static String positive = "positive";
+
+	// static List<String> matchList = new ArrayList<String>();
+	static Map<String, Integer[]> allEmoji = new HashMap<String, Integer[]>();
+
+	static Map<String, Integer[]> attributes = new HashMap<String, Integer[]>();
+
+	public static void main(String args[]) {
+		try {
+
+			String mode = "train";
+
+			Scanner decisionScan = new Scanner(new File(mode + "-labels.txt"));
+
+			Scanner scan = new Scanner(new File(mode + "-tweets.txt"));
+
+
+			Stemmer s = new Stemmer();
+
+			while (scan.hasNextLine()) {
+				String  decison = decisionScan.nextLine();
+				String decisionString = decison.split("\t")[1];
+
+				String line = scan.nextLine();
+				String[] array = line.split("\t");
+				// preprocessing
+				String content = array[1];
+				content = content.toLowerCase();
+				content = content.replace("\"", "");
+				content = content.replace("?", "");
+				content = content.replace(",", "");
+				content = content.replace("!", "");
+				content = content.replace(".", "");
+				// content = content.replace(":","");
+				array[1] = content;
+
+				// get the emoji
+				outputEmoji(array[1].replace("http://", "").replace("https://", ""), decisionString);
+
+				// for words
+				String[] words = array[1].split(" ");
+				for (String word : words ) {
+
+					// stemming
+					s.add(word.toCharArray(), word.length());
+					s.stem();
+					word = s.toString();
+
+
+					if (word.length() <= 0) {continue;}
+					// filter out mentions
+					if (word.charAt(0) == '@') {continue;}
+					
+					if (word.charAt(0) == '\\') {continue;}
+					// filter out topics
+					if (word.charAt(0) == '#') {continue;}
+					// filtrt out https
+					if (word.indexOf("http") == 0 ) {continue;}
+					if (Arrays.asList(stopWords).contains(word.toLowerCase())) {continue;}
+
+					AddToCount(attributes, word.toLowerCase().replace(":", ""), decisionString);
+
+					if (Arrays.asList(negationWords).contains(word.toLowerCase())) {
+						AddToCount(attributes, "NEGATIONWORD", decisionString);
+
+					}
+
+
+				}
+			}
+
+			// attributes = sortByValue(attributes);
+			System.out.println();
+			System.out.println(" Map Elements " + attributes.size());
+			// System.out.println("\t" + attributes);
+			printOutPersentage(attributes);
+
+			// allEmoji = sortByValue(allEmoji);
+
+			System.out.println(" Map Elements " + allEmoji.size());
+			printOutPersentage(allEmoji);
+
+			// System.out.println("\t" + allEmoji);
+		} catch (Exception e) {
+			e.printStackTrace();
+			//error handling code
+		}
+	}
+
+	public static void printOutPersentage(Map<String, Integer[]> map) {
+		Map<String, Double> percentage = new HashMap<String, Double>();
+		int thereshold = 5;
+		Iterator it = map.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, Integer[]> pair = (Map.Entry<String, Integer[]>)it.next();
+			if ((pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) > thereshold) {
+				double allApp = (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]);
+				// double perc = (pair.getValue()[0] + pair.getValue()[2]) * 1.00 / (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) + (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) * 0.0001;
+				// the pos and neg
+				// double perc = (pair.getValue()[0] + pair.getValue()[2]) * 1.00 / (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) + (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) * 0.0001;
+				
+				// the pos
+				double perc = (pair.getValue()[2]) * 1.00 / (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) + (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) * 0.0001;
+
+				// the neg
+				// double perc = (pair.getValue()[0]) * 1.00 / (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) + (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) * 0.0001;
+
+				// the nu
+				// double perc = (pair.getValue()[1]) * 1.00 / (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) + (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) * 0.0001;
+
+				// the gini
+				// double perc = 1 - ((pair.getValue()[0] / allApp)*(pair.getValue()[0] / allApp) ) - ((pair.getValue()[1] / allApp)*(pair.getValue()[1] / allApp) ) - ((pair.getValue()[2] / allApp)*(pair.getValue()[2] / allApp) );
+
+				// the frequency
+				// double perc = (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) + (pair.getValue()[0] + pair.getValue()[1] + pair.getValue()[2]) * 0.0001;
+
+				percentage.put(pair.getKey(), round(perc, 4) );
+
+			}
+			// System.out.println(pair.getKey() + " = " + pair.getValue()[0] + " " + pair.getValue()[1] + " " + pair.getValue()[2] + " ");
+			//it.remove(); // avoids a ConcurrentModificationException
+		}
+		percentage = sortByValue(percentage);
+
+
+		Iterator percentageIT = percentage.entrySet().iterator();
+		while (percentageIT.hasNext()) {
+			Map.Entry<String, Double> percPair = (Map.Entry<String, Double>)percentageIT.next();
+			Integer[] data = map.get(percPair.getKey());
+
+			System.out.println("allAttributes.add(new SimpleEntry(\"" + percPair.getKey() + "\", \"NUMERIC\"));" + "// = " + percPair.getValue() + " " + (!map.containsKey(percPair.getKey()) ? " " : (data[0] + " " + data[1] + " " + data[2] + " ")));
+			percentageIT.remove(); // avoids a ConcurrentModificationException
+		}
+		// System.out.println("\t" + percentage);
+	}
+
+	public static void printOutNumber(Map<String, Integer[]> map) {
+		Iterator it = map.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, Integer[]> pair = (Map.Entry<String, Integer[]>)it.next();
+			System.out.println(pair.getKey() + " = " + pair.getValue()[0] + " " + pair.getValue()[1] + " " + pair.getValue()[2] + " ");
+			it.remove(); // avoids a ConcurrentModificationException
+		}
+	}
+
+	// to sort the map
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue( Map<K, V> map ) {
+		List<Map.Entry<K, V>> list =
+		    new LinkedList<Map.Entry<K, V>>( map.entrySet() );
+		Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+			public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 ) {
+				// return (o2.getValue()[0] + o2.getValue()[1] + o2.getValue()[2]).compareTo(o1.getValue()[0] + o1.getValue()[1] + o1.getValue()[2]);
+				return (o2.getValue()).compareTo( o1.getValue() );
+			}
+		} );
+
+		Map<K, V> result = new LinkedHashMap<K, V>();
+		for (Map.Entry<K, V> entry : list) {
+			result.put( entry.getKey(), entry.getValue() );
+		}
+		return result;
+	}
+
+
+	// 	public static <String, Integer[] extends Comparable<? super Integer[]>> Map<String, Integer[]> sortByValue( Map<String, Integer[]> map ) {
+	// 	List<Map.Entry<String, Integer[]>> list =
+	// 	    new LinkedList<Map.Entry<String, Integer[]>>( map.entrySet() );
+	// 	Collections.sort( list, new Comparator<Map.Entry<String, Integer[]>>() {
+	// 		public int compare( Map.Entry<String, Integer[]> o1, Map.Entry<String, Integer[]> o2 ) {
+	// 			return (o2.getValue()[0]+o2.getValue()[1]+o2.getValue()[2]).compareTo(o1.getValue()[0]+o1.getValue()[1]+o1.getValue()[2]);
+	// 		}
+	// 	} );
+
+	// 	Map<String, Integer[]> result = new LinkedHashMap<String, Integer[]>();
+	// 	for (Map.Entry<String, Integer[]> entry : list) {
+	// 		result.put( entry.getKey(), entry.getValue() );
+	// 	}
+	// 	return result;
+	// }
+
+	public static void outputEmoji(String str, String decisionStr) throws Exception {
+		// String face = ":)";
+		String[] faces = {":-)", ":)", ":D", ":-D", ":P", ":-P", ":(", ":-/", ":/", ";)", "LOL", "HAHA"};
+
+		for (String face : faces) {
+			if (str.toUpperCase().contains(face)) {
+				AddToCount(allEmoji, face, decisionStr);
+				// System.out.println("Get a face" + face + " " + str);
+			}
+		}
+
+
+		String regexPattern = "[\uD83C-\uDBFF\uDC00-\uDFFF]";
+		byte[] utf8 = str.getBytes("UTF-8");
+
+		String string1 = new String(utf8, "UTF-8");
+
+		Pattern pattern = Pattern.compile(regexPattern);
+		Matcher matcher = pattern.matcher(string1);
+
+		while (matcher.find()) {
+			AddToCount(allEmoji, matcher.group(), decisionStr);
+		}
+
+	}
+
+	public static void AddToCount(Map<String, Integer[]> map, String key, String decisionStr) {
+		Integer[] countArray = new Integer[3];
+		countArray[0] = 0;
+		countArray[1] = 0;
+		countArray[2] = 0;
+
+		if (map.containsKey(key)) {
+			countArray = map.get(key);
+			// System.out.println("have the key " + key + " " + countArray[0] + " " + countArray[1] + " " + countArray[2] + " " + decisionStr);
+
+		} else {
+
+		}
+
+		if (decisionStr.equals(negative)) {
+			countArray[0] += 1;
+		} else if (decisionStr.equals(neutral)) {
+			countArray[1] += 1;
+		} else if (decisionStr.equals(positive) ) {
+			countArray[2] += 1;
+		}
+
+
+		map.put(key, countArray);
+	}
+
+	public static double round(double value, int places) {
+		if (places < 0) throw new IllegalArgumentException();
+
+		long factor = (long) Math.pow(10, places);
+		value = value * factor;
+		long tmp = Math.round(value);
+		return (double) tmp / factor;
+	}
+	public static String[] negationWords = {
+		"without", "no", "nor", "not", "cannot", "few",
+		"neither", "never", "nobody", "non", "none", "nothing", "nowhere",
+		"can't", "cannot", "couldn't", "didn't", "doesn't", "don't", "hadn't",
+		"hasn't", "haven't", "isn't", "mustn't",
+		"shan't", "shouldn't", "wasn't", "weren't", "won't", "wouldn't",
+		"aren't"
+	};
+
+	public static String[] stopWords = {
+		"see", "unless", "due", "also", "must", "might", "like",
+		//"]","[", "}", "{", "<", ">", "?", "\"", "\\", "/", ")", "(",
+		"will", "may",
+		"can", "much", "every", "the", "in", "other", "this", "the", "many", "any",
+		"an", "or", "for", "in", "an", "an ", "is", "a", "about", "above", "after",
+		"again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as",
+		"at", "be", "because", "been", "before", "being", "below", "between",
+		"both", "but", "by",  "could", "did",
+		"do", "does", "doing",  "down", "during", "each", "few",
+		"for", "from", "further", "had",  "has",  "have",
+		"having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
+		"herself", "him", "himself", "his", "how", "how's", "i ", " i", "i'd", "i'll",
+		"i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself",
+		"let's", "me", "more", "most",  "my", "myself",
+		"of", "off", "on", "once", "only", "ought", "our", "ours", "ourselves",
+		"out", "over", "own", "same",  "she", "she'd", "she'll", "she's",
+		"should",  "so", "some", "such", "than", "that", "that's", "their",
+		"theirs", "them", "themselves", "then", "there", "there's", "these", "they",
+		"they'd", "they'll", "they're", "they've", "this", "those", "through", "to",
+		"too", "under", "until", "up", "very", "was",  "we", "we'd",
+		"we'll", "we're", "we've", "were",  "what", "what's", "when",
+		"when's", "where", "where's", "which", "while", "who", "who's", "whom",
+		"why", "why's", "with", "would", "you", "you'd", "you'll",
+		"you're", "you've", "your", "yours", "yourself", "yourselves"
+	};
+}
